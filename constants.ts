e9c0b502f5baf5257cf7d747c56dc00d7de1d855
jperezvida <jperezvida@gmail.com>
2026-02-23 17:25:41 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -11,6 +11,6 @@ export const USERS: User[] = [
   { id: '1', name: 'Naima', email: 'naima@practicas.com', role: 'EDITOR', color: '#00BCD4' }, // Cyan
   { id: '2', name: 'David', email: 'david@practicas.com', role: 'EDITOR', color: '#4CAF50' }, // Verde
   { id: '3', name: 'Prácticas 3', email: 'p3@practicas.com', role: 'EDITOR', color: '#FF9800' }, // Naranja (hueco libre)
-  { id: '5', name: 'Cátedra', email: 'catedra@practicas.com', role: 'VIEWER', color: '#3F51B5' }, // Índigo
-  { id: '6', name: 'Ana Bedate', email: 'ana@practicas.com', role: 'VIEWER', color: '#E91E63' } // Rosa
+  { id: '5', name: 'Cátedra', email: 'catedra@practicas.com', role: 'VIEWER', color: '#9b9b9b' }, // Gris
+  { id: '6', name: 'Ana Bedate', email: 'ana@practicas.com', role: 'VIEWER', color: '#9b9b9b' } // Gris
 ];
