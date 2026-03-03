1f68cc1c1d0b3888eaaa8689729ff871323e720d
jperezvida <jperezvida@gmail.com>
2026-03-03 13:32:33 +0100

    Update constants.ts

--- constants.ts
+++ constants.ts
@@ -8,7 +8,7 @@ export const MONTHS = [
 export const DAYS_OF_WEEK = ["LUN", "MAR", "MIÉ", "JUE", "VIE", "SÁB", "DOM"];
 
 export const USERS: User[] = [
-  { id: '1', name: 'Naima', email: 'naima@practicas.com', role: 'EDITOR', color: '#00BCD4' }, // Cyan
+  { id: '1', name: 'Naimar', email: 'naima@practicas.com', role: 'EDITOR', color: '#00BCD4' }, // Cyan
   { id: '2', name: 'David', email: 'david@practicas.com', role: 'EDITOR', color: '#4CAF50' }, // Verde
   { id: '3', name: 'Pablo', email: 'pablo@practicas.com', role: 'EDITOR', color: '#FF9800' }, // Naranja
   { id: '4', name: 'Iria', email: 'iria@practicas.com', role: 'EDITOR', color: '#9C27B0' }, // Morado
